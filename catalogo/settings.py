8d70bbe8bbe93bf3221723badc66bf8d56fbc58c
Wlamir Lira <wlamirl@gmail.com>
2025-09-18 18:41:22 -0300

    Alterações no settings.py e requirements.txt, e adição do db.json

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -82,15 +82,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'catalogo.wsgi.application'
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
-else:
-    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+#DATABASE_URL = os.environ.get("DATABASE_URL")
+#if DATABASE_URL:
+#    DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
+#else:
+#    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
 #DATABASES = {
 #    'default': {
